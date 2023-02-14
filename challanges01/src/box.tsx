e90f2271d2308676e4e3586c010f674405bffba2
DESKTOP-7V9Q99B\AKU <akuraza6atgmail.com>
2023-02-14 16:32:16 +0500

    add new

--- challanges01/src/box.tsx
+++ challanges01/src/box.tsx
@@ -1,17 +1,31 @@
-import * as React from 'react';
+import * as React from "react";
 
- interface IAppProps {
-    arr:number[]
+interface IAppProps {
+  arr: number[];
 }
-
-export default function Box ({arr}:IAppProps) {
-    console.log(arr,'ARR')
+export default function Box({ arr }: IAppProps) {
   return (
-    <div >
-      {arr.map((ar) => (
-        <div style={{display:'flex',justifyContent:'space-between'}}>{ar}</div> 
-        ))} 
-      
+    <div style={{display:'flex',justifyContent:'space-between',marginLeft:'-300px', alignContent:'center' ,width:'400px'}}
+    //   style={{
+    //     gridTemplateColumns: "100px 50px 100px",
+    //     gridTemplateRows: " 80px auto 80px",
+    //     columnGap: "110px",
+    //     rowGap: "105px",
+    //     gap:'100px'
+    //   }}
+    >
+        <p style={{backgroundColor:'teal',padding:'40px',margin:'40px'}}>1</p>
+        <p style={{backgroundColor:'teal',padding:'40px',margin:'40px'}}>2</p>
+        <p style={{backgroundColor:'teal',padding:'40px',margin:'40px'}}>3</p>
+        <p style={{backgroundColor:'teal',padding:'40px',margin:'40px'}}>4</p>
+        <p style={{backgroundColor:'teal',padding:'40px',margin:'40px'}}>5</p>
+        <p style={{backgroundColor:'teal',padding:'40px',margin:'40px'}}>6</p>
+
+      {/* {arr.map((ar) =><p style={{ gridTemplateColumns: "100px 50px 100px",
+        gridTemplateRows: " 80px auto 80px",
+        columnGap: "110px",
+        rowGap: "105px",
+        gap:'100px'}}>{ ar}</p>)} */}
     </div>
   );
 }
